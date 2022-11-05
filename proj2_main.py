cff78a4ff007449fecb5477abb3a10758f64105a
Hrbibbi <97088090+Hrbibbi@users.noreply.github.com>
2022-11-05 19:06:18 +0100

    binann

--- proj2_main.py
+++ proj2_main.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import xlrd
 import scipy.linalg as linalg
-import array_to_latex as a2l
+#import array_to_latex as a2l
 import sklearn.linear_model as lm
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
@@ -223,7 +223,7 @@ def ANN_test_model(data, test_idx, net):
 ANN_model = ANN_train_model(data, np.arange(data.shape[0]), 5)
 
 ANN_test_model(data, np.arange(data.shape[0]), ANN_model)
-
+'''
 
 ### two-level CV
 Hs = np.array([1,5,10,15,20])
@@ -257,104 +257,6 @@ np.savetxt("ANN_data", ANN_data_arr)
 
 the_table[:,1] = min_Hs
 the_table[:,2] = E_test
-
-'''
-###
-# Parameters for neural network classifier
-n_hidden_units = 20     # number of hidden units
-n_replicates = 1        # number of networks trained in each k-fold
-max_iter = 10000
-
-# K-fold crossvalidation
-K = 3                   # only three folds to speed up this example
-CV = model_selection.KFold(K, shuffle=True)
-
-# Setup figure for display of learning curves and error rates in fold
-summaries, summaries_axes = plt.subplots(1,2, figsize=(10,5))
-# Make a list for storing assigned color of learning curve for up to K=10
-color_list = ['tab:orange', 'tab:green', 'tab:purple', 'tab:brown', 'tab:pink',
-              'tab:gray', 'tab:olive', 'tab:cyan', 'tab:red', 'tab:blue']
-# Define the model
-model = lambda: torch.nn.Sequential(
-                    torch.nn.Linear(M, n_hidden_units), #M features to n_hidden_units
-                    torch.nn.Tanh(),   # 1st transfer function,
-                    torch.nn.Linear(n_hidden_units, 1), # n_hidden_units to 1 output neuron
-                    # no final tranfer function, i.e. "linear output"
-                    )
-loss_fn = torch.nn.MSELoss() # notice how this is now a mean-squared-error loss
-
-print('Training model of type:\n\n{}\n'.format(str(model())))
-errors = [] # make a list for storing generalizaition error in each loop
-for (k, (train_index, test_index)) in enumerate(CV.split(X_normalized,y)): 
-    print('\nCrossvalidation fold: {0}/{1}'.format(k+1,K))    
-    
-    # Extract training and test set for current CV fold, convert to tensors
-    X_train = torch.Tensor(X_normalized[train_index,:])
-    y_train = torch.Tensor(y[train_index])
-    X_test = torch.Tensor(X_normalized[test_index,:])
-    y_test = torch.Tensor(y[test_index])
-    
-    # Train the net on training data
-    net, final_loss, learning_curve = train_neural_net(model,
-                                                       loss_fn,
-                                                       X=X_train,
-                                                       y=y_train,
-                                                       n_replicates=n_replicates,
-                                                       max_iter=max_iter)
-    
-    print('\n\tBest loss: {}\n'.format(final_loss))
-    
-    # Determine estimated class labels for test set
-    y_test_est = net(X_test)
-    
-    # Determine errors and errors
-    se = (y_test_est.float()-y_test.float())**2 # squared error
-    mse = (sum(se).type(torch.float)/len(y_test)).data.numpy() #mean
-    errors.append(mse) # store error rate for current CV fold 
-    
-    # Display the learning curve for the best net in the current fold
-    h, = summaries_axes[0].plot(learning_curve, color=color_list[k])
-    h.set_label('CV fold {0}'.format(k+1))
-    summaries_axes[0].set_xlabel('Iterations')
-    summaries_axes[0].set_xlim((0, max_iter))
-    summaries_axes[0].set_ylabel('Loss')
-    summaries_axes[0].set_title('Learning curves')
-
-# Display the MSE across folds
-summaries_axes[1].bar(np.arange(1, K+1), np.squeeze(np.asarray(errors)), color=color_list)
-summaries_axes[1].set_xlabel('Fold')
-summaries_axes[1].set_xticks(np.arange(1, K+1))
-summaries_axes[1].set_ylabel('MSE')
-summaries_axes[1].set_title('Test mean-squared-error')
-    
-print('Diagram of best neural net in last fold:')
-weights = [net[i].weight.data.numpy().T for i in [0,2]]
-biases = [net[i].bias.data.numpy() for i in [0,2]]
-tf =  [str(net[i]) for i in [1,2]]
-draw_neural_net(weights, biases, tf, attribute_names=attributeNames)
-
-# Print the average classification error rate
-print('\nEstimated generalization error, RMSE: {0}'.format(round(np.sqrt(np.mean(errors)), 4)))
-
-# When dealing with regression outputs, a simple way of looking at the quality
-# of predictions visually is by plotting the estimated value as a function of 
-# the true/known value - these values should all be along a straight line "y=x", 
-# and if the points are above the line, the model overestimates, whereas if the
-# points are below the y=x line, then the model underestimates the value
-plt.figure(figsize=(10,10))
-y_est = y_test_est.data.numpy(); y_true = y_test.data.numpy()
-axis_range = [np.min([y_est, y_true])-1,np.max([y_est, y_true])+1]
-plt.plot(axis_range,axis_range,'k--')
-plt.plot(y_true, y_est,'ob',alpha=.25)
-plt.legend(['Perfect estimation','Model estimations'])
-plt.title('Alcohol content: estimated versus true value (for last CV-fold)')
-plt.ylim(axis_range); plt.xlim(axis_range)
-plt.xlabel('True value')
-plt.ylabel('Estimated value')
-plt.grid()
-
-plt.show()
-###
 '''
 
 from scipy.stats import t
@@ -377,3 +279,74 @@ conf_int = [t.ppf(sig_level/2, nu), t.ppf(1-sig_level/2, nu)]
 t_hat = r_hat/s_tilde
 
 t.cdf(t_hat, nu)
+
+
+#binary ANN
+def ANN_train_model_b(data, train_idx, h_units):
+    n_hidden_units = h_units     # number of hidden units
+    n_replicates = 1        # number of networks trained in each k-fold
+    max_iter = 10000
+    
+    # Define the model
+    model = lambda: torch.nn.Sequential(
+                        torch.nn.Linear(M, n_hidden_units), #M features to H hiden units
+                        # 1st transfer function, either Tanh or ReLU:
+                        torch.nn.Tanh(),                            #torch.nn.ReLU(),
+                        torch.nn.Linear(n_hidden_units, 1), # H hidden units to 1 output neuron
+                        torch.nn.Sigmoid() # final tranfer function
+                        )
+    # Since we're training a neural network for binary classification, we use a 
+    # binary cross entropy loss (see the help(train_neural_net) for more on
+    # the loss_fn input to the function)
+    loss_fn = torch.nn.BCELoss()
+    # Extract training and test set for current CV fold, convert to tensors
+    X_train = torch.Tensor(data[train_idx,:-1])
+    y_train = torch.Tensor(np.expand_dims(data[train_idx,-1],axis=1))
+    
+    # Train the net on training data
+    net, final_loss, learning_curve = train_neural_net(model,
+                                                       loss_fn,
+                                                       X=X_train,
+                                                       y=y_train,
+                                                       n_replicates=n_replicates,
+                                                       max_iter=max_iter)
+    
+    return net
+def ANN_test_model_b(data, test_idx, net):
+    X_test = torch.Tensor(data[test_idx,:-1])
+    y_test = torch.Tensor(np.expand_dims(data[test_idx,-1],axis=1))
+     
+    # Determine errors and errors
+    y_sigmoid = net(X_test) # activation of final note, i.e. prediction of network
+    y_test_est = (y_sigmoid > .5).type(dtype=torch.uint8) # threshold output of sigmoidal function
+    y_test = y_test.type(dtype=torch.uint8)
+    # Determine errors and error rate
+    e = (y_test_est != y_test)
+    error_rate = (sum(e).type(torch.float)/len(y_test)).data.numpy()
+    return error_rate
+
+
+### two-level CV
+Hs = np.array([1,5,10,15,20])
+K1, K2 = 10,10
+outer_kf = KFold(n_splits=K1)
+E_val = np.empty((len(Hs), K2))
+E_test = np.empty((K1))
+min_Hs = np.empty((K1))
+outer_count = -1
+for par_idx, test_idx in outer_kf.split(data_bin):
+    outer_count += 1
+    inner_kf = KFold(n_splits=K2)
+    inner_count = -1
+    for train_idx, val_idx in inner_kf.split(par_idx):
+        inner_count += 1
+        for s in range(len(Hs)):
+            model = ANN_train_model_b(data_bin[par_idx], train_idx, Hs[s])
+            E_val[s,inner_count] = ANN_test_model_b(data_bin[par_idx], val_idx, model)
+    E_gen = np.mean(E_val, axis=1)
+    min_idx = np.argmin(E_gen)
+    min_Hs[outer_count] = Hs[min_idx]
+    model_star = ANN_train_model_b(data_bin, par_idx, Hs[min_idx])
+    E_test[outer_count] = ANN_test_model_b(data_bin, test_idx, model_star)
+    print(f"{round(100*(outer_count+1)/K1)}%")
+print(np.mean(E_test))
